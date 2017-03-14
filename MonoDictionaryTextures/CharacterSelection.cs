63ab1e638ba903ed6f82fb52be44976f0dcca58f
ITSPaul <powell.paul@itsligo.ie>
2017-03-14 13:59:55 +0000

    recommiting

---
+++ MonoDictionaryTextures/CharacterSelection.cs
@@ -0,0 +1,32 @@
+﻿using Engine.Engines;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoDictionaryTextures
+{
+    class CharacterSelection
+    {
+        public Texture2D Texture;
+        public Rectangle Bound;
+        public bool Selected = false;
+
+        public CharacterSelection(Texture2D tx, Vector2 pos)
+        {
+            Texture = tx;
+            Bound = new Rectangle(pos.ToPoint(), new Point(tx.Width, tx.Height));
+        }
+
+        public void Update()
+        {
+            
+            if(Bound.Contains(InputEngine.MousePosition) && InputEngine.IsMouseLeftClick())
+            {
+                Selected = true;
+            }
+        }
+    }
+}
---
+++ MonoDictionaryTextures/CharacterSelector.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoDictionaryTextures
+{
+    class CharacterSelector
+    {
+        Texture2D _background;
+        Dictionary<string, CharacterSelection> characters = 
+            new Dictionary<string, CharacterSelection>();
+
+        public CharacterSelector()
+        {
+            
+        }
+
+    }
+}
